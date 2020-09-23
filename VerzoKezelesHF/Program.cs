abe04ade454771cc96e5751a563602161c191e66
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-09-23 13:01:25 +0200

    A legnagyobb és legkisebb szám kiszámolása

--- VerzoKezelesHF/Program.cs
+++ VerzoKezelesHF/Program.cs
@@ -10,6 +10,8 @@ namespace VerzoKezelesHF
     {
         static void Main(string[] args)
         {
+            var szamok = new List<int>();
+            
             Console.Write("Kérem adja meg hány szám átlagát szeretné megadni: ");
             int N = int.Parse(Console.ReadLine());
             Console.Clear();
@@ -20,9 +22,13 @@ namespace VerzoKezelesHF
                 Console.Write("Kérem adja meg a(z) {0}. számot: ", i + 1);
                 int szam = int.Parse(Console.ReadLine());
                 sum += szam;
+                szamok.Add(szam);
             }
 
             Console.WriteLine("A számok átlaga: " + sum / N);
+            szamok.Sort();
+            Console.WriteLine("A legkisebb szám: {0}\n" +
+                "A legnagyobb: {1} ",szamok[0],szamok[szamok.Count-1]);
 
             Console.ReadKey();
         }
